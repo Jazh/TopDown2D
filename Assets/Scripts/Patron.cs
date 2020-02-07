d9f21c54265877d49735f8bc57c4ef905ed93d22
unknown <jzevallosh92@gmail.com>
2020-02-06 22:09:25 -0500

    update

--- Assets/Scripts/Patron.cs
+++ Assets/Scripts/Patron.cs
@@ -2,85 +2,149 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Patron : MonoBehaviour
+namespace TopDown.AI
 {
-    int currentIndex;
-    float t = 0f;
-    Vector3 startPos;
-    Vector3 endPos;
-    Vector3 point;
-    Vector3 origin;
-    public Vector2[] points;
-public ActionArea player;
-public float speed = 1f;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Patron : MonoBehaviour
     {
-                    currentIndex = 0;
-            point = points[currentIndex];
-            origin = transform.position;
-            startPos = origin;
-            endPos = origin + point;
+        int currentIndex;
+        float t = 0f;
+        Vector3 startPos;
+        Vector3 endPos;
+        private List<Vector3> points;
+ 
+       // Vector3 point;
+      //  Vector3 origin;
+      //  public Vector2[] points;
+        public ActionArea player;
+        public float speed = 1f;
+
+        public Path path;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+
+            currentIndex = 0;
+            //    point = points[currentIndex];
+            //  origin = transform.position;
+            points = path.points;
+
+            startPos = points[0];
+            endPos = points[1];
+
+            if (path.type == PathType.reset && path.isClosed) { 
+                points.Add(points[0]);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
             //Vector3 point = points[0];
             transform.position = Vector3.Lerp(startPos, endPos, t);
 
-           t += Time.deltaTime * speed;
-
-            if (Vector3.Distance(transform.position, player.transform.position) <= player.radius) {
+            t += Time.deltaTime * speed;
+            /*
+            if (Vector3.Distance(transform.position, player.transform.position) <= player.radius)
+            {
                 speed *= 2f;
-            }            else {
+            }
+            else
+            {
                 float distance = Vector3.Distance(startPos, endPos);
                 speed = 2f / distance;
-            }
+            }*/
 
-            if (t >= 1f){
-                
-                startPos = endPos;
+            if (t >= 1f)
+            {
                 currentIndex++;
-
-                if (currentIndex >= points.Length) {
+                /*
+                if (currentIndex >= points.Count)
+                {
                     currentIndex = -1;
                     point = Vector3.zero;
                 }
-                else {
+                else
+                {
                     point = points[currentIndex];
                 }
                 endPos = origin + point;
 
+                if (path.isClosed && currentIndex >= points.Count)
+                {
+                    currentIndex = 0;
+                }
+
+                endPos = points[currentIndex];*/
+
+                if (currentIndex >= points.Count) {
+                    OnFinishPath();
+                }
+
+                startPos = endPos;
+                endPos = points[currentIndex];
+
                 float distance = Vector3.Distance(startPos, endPos);
-                speed = 2f /distance;
+                speed = 2f / distance;
 
                 t = 0;
             }
-    }
-
-    private void OnDrawGizmos() {
-        if (!Application.isPlaying && origin != transform.position) {
-            origin = transform.position;
         }
-        Vector3 point = points[0];
-        //Debug.DrawLine(transform.position, points[0], Color.cyan, 0.1f);
-        //Debug.DrawLine(transform.position, transform.position + point, Color.cyan, 0.1f);
-        Debug.DrawLine(origin, origin + point, Color.cyan, 0.1f);
-        for (int i = 1; i < points.Length ; i++) {
-            //Debug.DrawLine
-            point = points[i-1];
-            Vector3 start = origin + point;
-            point = points[i];
-            Vector3 end = origin + point;
 
-            Debug.DrawLine(start, end, Color.cyan); 
+        private void OnFinishPath() {
+            switch (path.type) {
 
+                case PathType.loop:
+                    currentIndex = 0;
+                    if (!path.isClosed)
+                    {
+                        endPos = points[currentIndex];
+                        transform.position = endPos;
+                    }
+                    
+                    break;
+
+                case PathType.reset:
+                    points.Reverse();
+                    currentIndex = 0;
+                    break;
+                    
+            
+            }/*
+            if (path.isClosed) {
+                currentIndex = 0;
+            }
+            endPos = points[currentIndex];*/
         }
-        point = points[points.Length - 1];
-        Debug.DrawLine(origin + point, origin, Color.cyan);
+
+
+        /*
+        private void OnDrawGizmos()
+        {
+            if (!Application.isPlaying && origin != transform.position)
+            {
+                origin = transform.position;
+            }
+            Vector3 point = points[0];
+            //Debug.DrawLine(transform.position, points[0], Color.cyan, 0.1f);
+            //Debug.DrawLine(transform.position, transform.position + point, Color.cyan, 0.1f);
+            Debug.DrawLine(origin, origin + point, Color.cyan, 0.1f);
+            for (int i = 1; i < points.Length; i++)
+            {
+                //Debug.DrawLine
+                point = points[i - 1];
+                Vector3 start = origin + point;
+                point = points[i];
+                Vector3 end = origin + point;
+
+                Debug.DrawLine(start, end, Color.cyan);
+
+            }
+            point = points[points.Length - 1];
+            Debug.DrawLine(origin + point, origin, Color.cyan);
+
+        }*/
 
     }
-
 }
