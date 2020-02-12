ed8fb7e9506a8d4b51cea661117bbbe84074f159
unknown <jzevallosh92@gmail.com>
2020-02-11 22:05:21 -0500

    update game

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDown
+{
+    public class Attack : MonoBehaviour
+    {
+
+        public GameObject fire;
+        public GameObject ice;
+        public int currentAttack;
+        [System.Serializable]
+        public struct Attacks
+        {
+            public ElementType type;
+            public Projectile prefab;
+        }
+
+        public Attacks[] attacksArray;
+
+        public Dictionary<ElementType, Projectile> attacks;
+        // Start is called before the first frame update
+        void Start()
+        {
+            attacks = new Dictionary<ElementType, Projectile>();
+
+            foreach (Attacks a in attacksArray)
+            {
+                attacks.Add(a.type, a.prefab);
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1)) ;
+            Shoot(1);
+            if (Input.GetKeyDown(KeyCode.Alpha2)) ;
+            Shoot(2);
+
+            if (Input.GetKeyDown(KeyCode.KeypadMinus))
+            {
+                currentAttack--;
+            }
+
+            if (Input.GetKeyDown(KeyCode.KeypadPlus))
+            {
+                currentAttack++;
+            }
+
+            currentAttack = Mathf.Clamp(currentAttack, 0, attacks.Count - 1);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Shoot(currentAttack);
+            }
+        }
+
+        public void Shoot(int type)
+        {
+            ElementType t = (ElementType)type;
+            Shoot(t);
+        }
+
+        public void Shoot(ElementType type)
+        {
+            Instantiate(attacks[type], transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TopDown { 
+public class Enemy : MonoBehaviour
+{
+        public ElementType type = ElementType.fire;
+        public ElementType weak = ElementType.fire;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Projectile projectile = collision.GetComponent<Projectile>();
+
+            if (projectile != null)
+                if (projectile.type == weak)
+                    Destroy(gameObject);
+
+        }
+}
+}
